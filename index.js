0e5a7eb1922763e0193b0842d7f47ecc7208c123
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-01-11 00:31:53 +1100

    Initial web scrapping using node

---
+++ index.js
@@ -0,0 +1,18 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+// Save a file
+const fs = require("fs");
+
+async function main() {
+  const html = await request.get(
+    "https://reactnativetutorial.net/css-selectors/"
+  );
+  fs.writeFileSync("./test.html", html);
+
+  const $ = await cheerio.load(html);
+  const theText = $("h1").text();
+  console.log(theText);
+}
+
+main();
